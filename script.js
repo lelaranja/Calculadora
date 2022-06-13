c129ada1023c2d1a53d7a0442742ce7760c0e5a6
lelaranja <leticiajlaranja@gmail.com>
2022-06-13 11:44:03 -0300

    add: adicionei id aos botões para uso no DOM

---
+++ script.js
@@ -0,0 +1,7 @@
+const botoesNumericos = document.querySelectorAll("#numeros");
+const operadoresNumericos = document.querySelectorAll("#operadoresC");
+const operadorIgual = document.querySelector("#igual");
+const botaoDel = document.querySelector("#delete");
+const delAll = document.querySelector("#delAll");
+const prevResult = document.querySelector("#preview");
+const valorAtual = document.querySelector("#atValue");
