57fa43a0bab96b5e939fb557632fd481f3b73f7b
lelaranja <leticiajlaranja@gmail.com>
2022-06-13 11:51:33 -0300

    add: criei classe calculadora e método p/ AC e DEL

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ const botaoDel = document.querySelector("#delete");
 const delAll = document.querySelector("#delAll");
 const prevResult = document.querySelector("#preview");
 const valorAtual = document.querySelector("#atValue");
+
+class Calculadora {
+  constructor(prevResult, valorAtual) {
+    this.prevResult = prevResult;
+    this.valorAtual = valorAtual;
+  }
+
+  limpar() {
+    this.prevResult = "";
+    this.valorAtual = "";
+    this.operadoresNumericos = undefined;
+  }
+  atualizaValor() {
+    this.prevResult.innerText = this.prevResult;
+    this.valorAtual.innerText = this.valorAtual;
+  }
+}
+
+const calculadora = new Calculadora(prevResult, valorAtual);
+
+delAll.addEventListener("click", () => {
+  this.limpar();
+  this.atualizaValor();
+});
